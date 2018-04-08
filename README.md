9947b4069d60aab8bd6c92c7063bb97b1a34a241
Peter Kasza <peter.kasza@itinsight.hu>
2018-04-08 18:05:11 +0200

    import of project

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # proxycheck
 Test HTTP(S) proxies for their utility and latency
+
+## Quickstart
+~~~~
+% python proxycheck.py
+usage: proxycheck.py [-h] [-l {WARNING,INFO,DEBUG}] [-t THREADS] [-u TESTURL]                                                        
+                     [-s TIMEOUT] [-r REPEATS]
+                     proxy_list
+proxycheck.py: error: the following arguments are required: proxy_list
+
+% python proxycheck.py proxylist
+INFO:root:Testing using url: https://www.akamai.com/robots.txt
+INFO:root:stdout is a terminal, suppressing CSV output
+INFO:root:<  207.154.244.19:3128  > is not responding
+INFO:root:<   188.244.5.229:3128  > is not responding
+INFO:root:< 195.201.129.209:3128  > is not responding
+INFO:root:< 195.201.129.209:3128  > is not responding
+INFO:root:<   60.248.220.22:1080  > is not responding
+INFO:root:<    91.82.85.154:3128  > has a latency of    19.27 ms (±   10.83 ms)
+INFO:root:<     167.99.42.6:8080  > has a latency of    20.19 ms (±    0.92 ms)
+INFO:root:<    176.9.40.189:3128  > has a latency of    24.89 ms (±    0.47 ms)
+INFO:root:< 178.238.228.187:9090  > has a latency of    22.00 ms (±    1.01 ms)
+INFO:root:<  80.211.135.118:3128  > is not responding
+INFO:root:<    51.254.45.80:3128  > has a latency of    29.92 ms (±    0.42 ms)
+INFO:root:<   87.98.174.157:3128  > has a latency of    32.33 ms (±    0.41 ms)
+INFO:root:<  212.237.57.154:3128  > is not responding
+INFO:root:<  80.211.228.238:8888  > has a latency of    38.90 ms (±    1.24 ms)
+INFO:root:<   81.177.23.240:8888  > is not responding
+INFO:root:<  212.237.26.124:8888  > has a latency of    37.52 ms (±    0.83 ms)
+~~~~
+
+## F.A.Q.
+### How to save the output in CSV format?
+To save the output in CSV, simply redirect the stdout stream to a file.
+For example run: `% python proxycheck.py proxylist > output.csv`
---
+++ proxycheck.py
@@ -0,0 +1,105 @@
+import argparse
+import hashlib
+import logging
+import requests
+import socket
+import statistics
+import sys
+import threading
+import timeit
+
+from queue import Queue
+
+class Worker(threading.Thread):
+    def __init__(self, queue, testurl, urlhash, timeout, repeats):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.testurl = testurl
+        self.urlhash = urlhash
+        self.timeout = timeout
+        self.repeats = repeats
+
+    def evalproxy(self, host, port):
+        def fetch():
+            proxy = {'http': 'http://%s:%d' %(host, port)}
+            proxy['https'] = proxy['http']
+
+            try:
+                response = requests.get(self.testurl, proxies=proxy, timeout=self.timeout)
+                response.raise_for_status()
+                return hashlib.md5(response.content).digest() == self.urlhash
+            except Exception as ex:
+                return False
+
+        if fetch():
+            samples = timeit.repeat(fetch, number=1, repeat=self.repeats)
+            return (100.0 * statistics.mean(samples),
+                    100.0 * statistics.stdev(samples))
+        else:
+            return None
+
+    def run(self):
+        while True:
+            host, port = self.queue.get()
+
+            host = str(socket.gethostbyname(host))
+            port = int(port)
+            
+            result = self.evalproxy(host, port)
+            if result != None:
+                logging.info('< %15s:%-5d > has a latency of %8.2f ms (±%8.2f ms)' %(host, port, result[0], result[1]))
+                if not sys.stdout.isatty():
+                    print('%s,%d,%f,%f' %(host, port, result[0], result[1]))
+                    sys.stdout.flush()
+            else:
+                logging.info('< %15s:%-5d > is not responding' %(host, port))
+            self.queue.task_done()
+
+def slurp(path):
+    with open(path, 'r') as fp:
+        return (line.rstrip() for line in fp.readlines())
+
+def main(arguments):
+    queue = Queue()
+    for line in slurp(arguments.proxy_list):
+        host, port = line.split(':')
+        queue.put((host, int(port)))
+
+    for k in range(arguments.threads):
+        thread = Worker(queue, arguments.testurl, arguments.urlhash,
+                        arguments.timeout, arguments.repeats)
+        thread.setDaemon(True)
+        thread.start()
+
+    queue.join()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test HTTP proxies for their utility and latency')
+    parser.add_argument('-l', '--loglevel', default='INFO', choices=['WARNING', 'INFO', 'DEBUG'],
+                        help='logging level to use')
+    parser.add_argument('-t', '--threads', default=10, type=int,
+                        help='number of threads to use')
+    parser.add_argument('-u', '--testurl', default='https://www.akamai.com/robots.txt', type=str,
+                        help='test url for latency measurement')
+    parser.add_argument('-s', '--timeout', default=5, type=int,
+                        help='number of seconds to wait before timeout')
+    parser.add_argument('-r', '--repeats', default=3, type=int,
+                        help='number of times to repeat measurement')
+    parser.add_argument('proxy_list', type=str,
+                        help='proxies to test in IP:PORT format')
+    arguments = parser.parse_args()
+
+    logging.basicConfig(level=arguments.loglevel)
+
+    arguments.urlhash = hashlib.md5(
+        requests.get(arguments.testurl).content
+    ).digest()
+
+    logging.info('Testing using url: %s' %(arguments.testurl))
+    if sys.stdout.isatty():
+        logging.info('stdout is a terminal, suppressing CSV output')
+    else:
+        print('host,port,mean,stdev')
+        sys.stdout.flush()
+    main(arguments)
+
